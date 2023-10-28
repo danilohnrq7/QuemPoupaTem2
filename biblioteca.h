6b20dd07bdc064487b81891d98d0bbbf7e41494a
PedroHCorreia <149112597+PedroHCorreia@users.noreply.github.com>
2023-10-28 00:59:22 -0300

    Atualizando os dados da struct Cliente
    Aumentando o limite de caracteres de "nome" e modificado o tipo de dado de "saldo" para float.

--- biblioteca.h
+++ biblioteca.h
@@ -3,10 +3,10 @@
 
 //Definindo a struct do cadastro de um clientes
 typedef struct {
-    char nome[30];
+    char nome[60];
     char cpf[12];
     char tipo_conta[7];
-    char valor_inicial[12];
+    float saldo;
     char senha[11];
 }Cliente; // Nomeando este tipo de struct como: Cliente
 
